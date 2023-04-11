f297e94ca7d259251e8d77f62f8975732303babf
Brandon Hsu <brandon.j.hsu@gmail.com>
2023-04-11 16:17:49 -0700

    changed App layout

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Modal, TouchableOpacity } from 'react-native';
+// import CheckBox from '@react-native-community/checkbox';
+import Checkbox from 'expo-checkbox';
 import DatePicker, { getToday, getFormatedDate } from 'react-native-modern-datepicker';
 
 export default function App() {
+
+
 
   const todayDate = new Date();
   const startDate = getFormatedDate(todayDate.setDate(todayDate.getDate()), 'YYYY/MM/DD');
 
   const [date, setDate] = useState();
   const [time, setTime] = useState();
-  const [text, setText] = useState();
+  const [text, setText] = useState('');
+  const [completed, setCompleted] = useState(false);
   const [openDateTimeModal, setOpenDateTimeModal] = useState(false);
   const [list, setList] = useState([])
+  const [counter, setCounter] = useState(0);
 
   const handleOpenDateTimeModal = () => {
     setOpenDateTimeModal(!openDateTimeModal);
@@ -30,36 +36,82 @@ export default function App() {
     setText(newText);
   }
 
+  const handleCompletion = (id) => {
+    setCompleted(!completed)
+  }
+
   const saveReminder = () => {
+    let day = date.substring(8);
+    let month = date.substring(5,7);
+    let year = date.substring(0,4);
+    let formatDate = (month + '/' + day + '/' + year);
+    let hour = time.substring(0,2);
+    let minute = time.substring(3);
+    let sortDate = new Date (year, month, day, hour, minute)
+
     let reminder = {
-      date: date,
+      id: counter,
+      date: formatDate,
       time: time,
-      text: text
+      text: text,
+      completed: false,
+      sortDate: sortDate,
     }
-    let copy = list.slice();
-    copy.push(reminder)
-    setList(copy)
+    let increment = counter + 1;
+    setCounter(increment);
+
+    let copyList = list.slice();
+    copyList.push(reminder)
+
+    //sort list by date --> time --> length of text --> reminder id
+    copyList.sort((a,b) => {
+      if (b.sortDate - a.sortDate) {
+        return a.sortDate - b.sortDate
+      }
+      if (b.sortDate === a.sortDate) {
+        if (b.text.length - a.text.length) {
+          return a.text.length - b.text.length
+        } else {
+          return a.id - b.id
+        }
+      }
+    })
+
+    //set the list of remminders and reset all values of the reminder
+    setList(copyList)
     setDate()
     setTime()
-    setText()
+    setText('')
   }
 
   return (
     <View style= {styles.container}>
-      <Text>Reminder: </Text>
+      <View style= {styles.title}>
+        <Text>
+          Reminder App
+        </Text>
+      </View>
 
-      <TextInput
-        style= {[styles.input, styles.reminder]}
-        onChangeText = {handleChangeText}
-      />
+      <View style= {styles.userInput}>
+        <Text>Reminder: </Text>
 
-      <TouchableOpacity onPress= {handleOpenDateTimeModal} style= {styles.button}>
-        <Text>Open</Text>
-      </TouchableOpacity>
+        <TextInput
+          style= {[styles.input, styles.reminder]}
+          onChangeText = {handleChangeText}
+          value= {text}
+        />
 
-      {date && time && text && <TouchableOpacity onPress= {saveReminder} style= {styles.button}>
-        <Text>Save</Text>
-      </TouchableOpacity>}
+        <View style= {styles.buttonRow}>
+          <TouchableOpacity onPress= {handleOpenDateTimeModal} style= {styles.button}>
+            <Text>Select Date and Time</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress= {saveReminder} style= {(!date || !time || !text) ? styles.buttonDisabled : styles.button} disabled= {!date || !time || !text}>
+            <Text>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
 
       <Modal
       animationType= 'slide'
@@ -74,20 +126,32 @@ export default function App() {
             onDateChange= {handleChangeDate}
             onTimeChange = {handleChangeTime}
           />
-            <TouchableOpacity onPress= {handleOpenDateTimeModal}>
+            <TouchableOpacity onPress= {handleOpenDateTimeModal} style= {(date && time) ? styles.button : null}>
               {date && time && <Text>Select</Text>}
             </TouchableOpacity>
           </View>
         </View>
       </Modal>
 
-      {list.map((item) =>
 
-          <Text>{item.date} {item.time} {item.text}</Text>
+      <View style= {styles.reminderList}>
+        {list.map((item) =>
+          <View style= {styles.reminderLayout} key= {item.id}>
+            <Checkbox
+              value= {item.completed}
+              onValueChange= {() => !item.completed}
+              style= {styles.checkbox}
+            />
+            <View >
+              <Text>{item.date} @ {item.time}</Text>
+              <Text>{item.text} {item.id}</Text>
 
-      )}
+            </View>
+          </View>
+        )}
+      </View>
+
       <StatusBar style="auto" />
-
     </View>
 
   );
@@ -95,10 +159,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    borderWidth: 2,
+    borderColor: 'red',
+  },
+
+  title: {
+    flex: 1,
+    marginTop: 70,
+  },
+
+  userInput: {
+    flex: 2,
+    marginTop: 20,
+  },
+
+  reminderList: {
+    flex: 5,
+    marginTop: 20,
   },
 
   input: {
@@ -131,7 +214,36 @@ const styles = StyleSheet.create({
 
   button: {
     margin: 20,
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 5,
+  },
+
+  reminderLayout: {
+    flexDirection: 'row',
+    columnGap: 10,
+    marginBottom: 20,
+  },
+
+  checkbox: {
+
+  },
+
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+
+  buttonDisabled: {
+    margin: 20,
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 5,
+    backgroundColor: 'grey',
+
   }
-
-
 });
+
+// {date && time && text && <TouchableOpacity onPress= {saveReminder} style= {styles.button}>
+//         <Text>Save</Text>
+//       </TouchableOpacity>}
